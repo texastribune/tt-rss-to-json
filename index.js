42deae20141d2dc3d2405681b9cf184bfbaf61dd
Ryan Murphy <rdmurphy@users.noreply.github.com>
2015-12-15 16:59:22 -0600

    For loop with callback fixed

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ var feedToJson = require('./convert')
 var source = require('./source')
 var uploadToS3 = require('./publish')
 
+function convertFeed (slug, url) {
+  feedToJson(url, function (data) {
+    uploadToS3(data, slug)
+  })
+}
+
 for (var feed in source) {
   if (source.hasOwnProperty(feed)) {
     var slug = feed
     var url = source[slug]
 
-    feedToJson(url, function (data) {
-      uploadToS3(data, slug)
-    })
+    convertFeed(slug, url)
   }
 }
